76709b092593f4d2d363294e8d1211253aa31d54
Aziz Kurniawan <aziz.kurniawan13@gmail.com>
2021-05-23 12:44:35 +0700

    latihan js awal

---
+++ latihan.js
@@ -0,0 +1,8 @@
+function tambah(a, b) {
+    return a + b;
+}
+
+var a = parseInt(prompt('Masuk nilai a : '));
+var b = parseInt(prompt('Masukan nilai b : '));
+var result = tambah(a, b);
+console.log(result);

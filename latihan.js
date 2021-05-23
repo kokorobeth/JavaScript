da461b56f223d4b3bc46cce37d2c8fbc49cc73b0
Aziz Kurniawan <aziz.kurniawan13@gmail.com>
2021-05-23 12:48:32 +0700

    menghapus file kosong

--- latihan.js
+++
@@ -1,8 +0,0 @@
-function tambah(a, b) {
-    return a + b;
-}
-
-var a = parseInt(prompt('Masuk nilai a : '));
-var b = parseInt(prompt('Masukan nilai b : '));
-var result = tambah(a, b);
-console.log(result);
